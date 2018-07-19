0aa5a19336ef6c5bae0776e426439a6f6b0ad818
Chintan N. Dave <cndave@analytix.com>
2018-07-19 22:40:05 +0530

    Cors middleware added

--- index.js
+++ index.js
@@ -253,8 +253,17 @@ Promise.all([MONGODATABSE.connectFunc(bluebird), REDISDATABSE(bluebird)])
         }
       }
 
-      app.all("*", function (req, res, next) {
+      app.options("/*", function(req, res, next) {
+        var origin = req.headers['Origin'];
+        origin = origin || '*';
+        res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, Content-Length, X-Requested-With, X-HTTP-Method-Override');
+        res.send(200);
+      });
 
+      app.all("*", function (req, res, next) {
         var token = req.csrfToken();
         res.cookie('XSRF-TOKEN', token);
         res.locals.csrfToken = token;
