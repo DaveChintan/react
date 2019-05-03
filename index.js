ff7b491da5e7de6078ca3b51e762700b1b9e7c5c
Chintan N. Dave <cndave@analytix.com>
2019-05-03 15:02:33 +0530

    Index.js added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require("http");
 
+var port = process.env.PORT || 8080;
+
 http
   .createServer(function(req, res) {
     res.writeHead(200, "Welcome", { "Content-Type": "text/html" });
     res.write("Welcome");
     res.end();
   })
-  .listen(8080);
+  .listen(port);
