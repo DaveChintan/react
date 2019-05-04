5e29bfa2dc40bd1428ba7cfa5e0f07097f6e597d
DaveChintan <davechintan11@gmail.com>
2019-05-04 21:17:11 +0530

    basic routes added

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 const http = require("http");
+const express = require('express');
+
+const app = express();
 
 var port = process.env.PORT || 8080;
 
-http
-  .createServer(function(req, res) {
+app.get('/', async function(req,res,next) {
     res.writeHead(200, "Welcome", { "Content-Type": "text/html" });
-    res.write("Welcome");
+    res.write("Root");
     res.end();
-  })
+});
+
+app.get('/index', async function(req,res,next) {
+  res.writeHead(200, "Welcome", { "Content-Type": "text/html" });
+  res.write("Index");
+  res.end();
+});
+
+function clientErrorHandler(err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send({ error: 'Something failed!' });
+  } else {
+    next(err);
+  }
+}
+
+function errorHandler (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render('error', { error: err });
+}
+
+app.use(clientErrorHandler);
+app.use(errorHandler);
+
+// http
+//   .createServer(function(req, res) {
+//     res.writeHead(200, "Welcome", { "Content-Type": "text/html" });
+//     res.write("Welcome");
+//     res.end();
+//   })
+//   .listen(port);
+
+http.createServer(app)
   .listen(port);
